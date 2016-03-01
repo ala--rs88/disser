ee94ac201f5d00c9016413efe720081d851361e0
Igor Karpov <igork.fet@gmail.com>
2016-03-01 21:56:29 +0300

    * GLCM descriptor now calculates semi-circular occurences.

--- brodatz_db__descriptors/descriptor_builders/glcm_descriptor_builder.py
+++ brodatz_db__descriptors/descriptor_builders/glcm_descriptor_builder.py
@@ -1,5 +1,6 @@
 from abstract_descriptor_builder import AbstractDescriptorBuilder
 from skimage.feature import greycomatrix
+from numpy import pi
 
 __author__ = 'IgorKarpov'
 
@@ -17,5 +18,11 @@ class GLCMDescriptorBuilder(AbstractDescriptorBuilder):
         return self.__descriptor_length
 
     def build_descriptor(self, image):
-        glcm_descriptor = greycomatrix(image, [5], [0], self.__image_depth, symmetric=True, normed=False).flatten()
+        glcms = greycomatrix(image,
+                             [2],
+                             [0, (1./4)*pi, (1./2)*pi, (3./4)*pi],
+                             self.__image_depth,
+                             symmetric=False,
+                             normed=False)
+        glcm_descriptor = (glcms[:, :, 0, 0] + glcms[:, :, 0, 1] + glcms[:, :, 0, 2] + glcms[:, :, 0, 3]).flatten()
         return glcm_descriptor
